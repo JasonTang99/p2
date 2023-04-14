Fix epsilon calculation, add visualization

evaluate_metrics.py
@@ -22,7 +22,7 @@ from opacus import PrivacyEngine
 from opacus.validators import ModuleValidator
 
 from utils import generate_run_id, get_input_args, Args, parse_run_id
-from models import Discriminator_FC, Generator_MNIST, Weight_Clipper, G_weights_init, Generator_FC
+from models import Discriminator_FC, Generator_MNIST, Weight_Clipper, G_weights_init, Generator_FC, Decoder_Mini, Encoder_VAE, Decoder_VAE, VAE
 from data import load_MNIST
 from metrics import get_IS, get_FID
 from model_inversion import enc_fp, dec_fp, gen_fp
@@ -38,7 +38,7 @@ def last_num_models(run_fp, num=10, query="netG"):
     return models
 
 # Calculate FIDs of last 10 models for latent GAN using WGAN
-def calculate_FIDs_WGAN(args, run_fp, num=10):
+def calculate_FIDs_WGAN(args, run_fp):
     # Load Public WGAN Generator
     pub_G = Generator_FC(hidden_sizes=[256], nz=100).to(device)
     pub_G.load_state_dict(torch.load(gen_fp))
@@ -60,12 +60,6 @@ def calculate_FIDs_WGAN(args, run_fp, num=10):
         G.load_state_dict(torch.load(model_fp))
         G.eval()
 
-        # Sample 4 fake latents
-        # noise = torch.randn(4, args.nz).to(device)
-        # fake = G(noise)
-        # print(fake)
-        # continue
-
         # Generate 2048 fake images
         noise = torch.randn(2048, args.nz).to(device)
         fake = pub_G(G(noise))
@@ -83,46 +77,88 @@ def calculate_FIDs_WGAN(args, run_fp, num=10):
 
     return best_model_fp, best_FID
 
+def calculate_AE(args, run_fp):
+    # Load Public Decoder
+    pub_Dec = Decoder_Mini(latent_size=100).to(device)
+    pub_Dec.load_state_dict(torch.load(dec_fp))
+
+    # Consider final model
+    model = last_num_models(run_fp, num=1, query="vae")[0]
+    gen_fp = os.path.join(run_fp, model)
+    print("Loading {}".format(gen_fp))
+
+    vae = VAE(
+        Encoder_VAE(args.hidden, latent_size=args.nz), 
+        Decoder_VAE(args.hidden, latent_size=args.nz)
+    ).to(device)
+    vae.load_state_dict(torch.load(gen_fp))
+    vae.eval()
+
+    G = vae.decoder
+    G.eval()
+
+    # Generate 2048 fake images
+    noise = torch.randn(2048, args.nz).to(device)
+    fake = pub_Dec(G(noise))
+    fake = fake.view(fake.size(0), 1, 28, 28)
+
+    # Calculate IS
+    FID = get_FID(fake)
+    # IS = get_IS(fake)
+
+    return FID
+
+
 
 if __name__ == "__main__":
-    # run_id = "/home/jason/p2/runs_gen_fc_3/public_128_100_32_1_inf_1e-06_0.0_0.01_5e-05_0.0_64_3_500000_LeakyReLU_0.0"
-    # run_id = "/home/jason/p2/runs_gen_fc_3/public_256_100_32_1_inf_1e-06_0.0_0.01_5e-05_0.0_64_3_500000_LeakyReLU_0.0"
-    
-    
-    # run_id = "runs_latent/ae-enc_64_64_32_1_50.0_1e-06_0.0_0.01_5e-05_0.5_64_3_100000_LeakyReLU_0.0"
-    
     # Save results to a pandas dataframe
     # Cols:     hiddens = [[96], [64]]
     # noise_multipliers = [0.0, 0.1]
     # activations = ["Tanh", "LeakyReLU", ]
     # n_ds = [3, 5]
     # c_ps = [0.01, 0.02]
+
     df = pd.DataFrame(columns=[
-        "hiddens", "noise_multiplier", "activation", 
-        "n_d", "c_p", "FID", "model_fp"
+        "hidden", "noise_multiplier", "activation", 
+        "n_g", "c_p", "lr", "IS", "latent_type", "model_fp"
     ])
 
-    folder = "runs_latent"
-    for run_id in os.listdir(folder):
+    folder = "runs_vae"
+    valid_runs_ids = []
+    for run_id in os.listdir("runs_vae"):
+        run_fp = f"runs_vae/{run_id}"
+        args = parse_run_id(run_id)
+        if run_id.startswith("ae-grad") and \
+                args.noise_multiplier > 0 and \
+                args.n_g > 20000 and \
+                args.n_g <= 50000:
+            valid_runs_ids.append(run_id)
+    valid_runs_ids = sorted(valid_runs_ids)
+
+    # Iterate over all runs
+    for run_id in tqdm(valid_runs_ids):
         run_fp = os.path.join(folder, run_id)
         args = parse_run_id(run_id)
+        print(args.noise_multiplier, args.c_p)
 
-        # Calculate FIDs
-        best_model_fp, best_FID = calculate_FIDs_WGAN(args, run_fp, num=1)
+        # Calculate FID
+        FID = calculate_AE(args, run_fp)
 
         # Save results
         df = df.append({
-            "hiddens": args.hidden,
+            "hidden": args.hidden,
             "noise_multiplier": args.noise_multiplier,
             "activation": args.activation,
-            "n_d": args.n_d,
+            "n_g": args.n_g,
             "c_p": args.c_p,
-            "FID": best_FID,
-            "model_fp": best_model_fp
+            "lr": args.lr,
+            "FID": FID,
+            "latent_type": "ae-grad",
+            "model_fp": run_fp
         }, ignore_index=True)
 
     # Save results
-    df.to_csv("latent_results.csv", index=False)
+    df.to_csv("vae_FID.csv", index=False)
 
     # Print results
     print(df)

train_dp_vae.py
@@ -60,6 +60,7 @@ def train_vae(args, vae, train_loader, run_fp="runs_vae/test", kld_weight=0.0005
 
     # Track time
     start_time = time()
+    eps = 0
 
     vae.train()
     with open(f"{run_fp}/loss.txt", "a") as f:
@@ -84,11 +85,18 @@ def train_vae(args, vae, train_loader, run_fp="runs_vae/test", kld_weight=0.0005
             loss.backward()
             optimizer.step()
             
-            print(f"{i}, {loss.item()}", file=f)
+            # print(f"{i}, {loss.item()}", file=f)
             
             if (i+1) % 200 == 0:
-                torch.save(vae._module.state_dict(), f"{run_fp}/vae_{i+1}.pt")
-                torch.save(privacy_engine.accountant, f"{run_fp}/accountant_{i+1}.pt")
+                # torch.save(vae._module.state_dict(), f"{run_fp}/vae_{i+1}.pt")
+                # torch.save(privacy_engine.accountant, f"{run_fp}/accountant_{i+1}.pt")
+                
+                f.flush()
+                if i < 10000 and eps < 300:
+                    # Print epsilon every 200 iterations
+                    eps = privacy_engine.get_epsilon(1e-6)
+                    print(f"Epsilon {i+1} {eps}", file=f)
+                    print(f"Epsilon {i+1} {eps}")
 
     print(f"Training took {time() - start_time} seconds")
 
@@ -137,8 +145,8 @@ def main(args, private=True, latent_type="ae_enc"):
             data_fp="data/wgan_latent_dataset.pt")
     else:
         raise ValueError("Latent type not supported")
-    
-    run_fp = os.path.join('runs_vae/', run_id)
+
+    run_fp = os.path.join('runs_vae_eps/', run_id)
     os.makedirs(run_fp, exist_ok=True)
 
     print(f"================= Run ID: {run_id} =================")
@@ -150,56 +158,75 @@ def main(args, private=True, latent_type="ae_enc"):
 def grid_search():
     # Private model Hyperparameter Search
     hiddens = [64]
-    noise_multipliers = [0.0, 0.05, 0.1, 0.2]
-    activations = ["Tanh", "LeakyReLU", ]
-    c_ps = [1.0, 0.5, 0.1, .05]
-    lrs = [0.01,] #  0.005]
+    noise_multipliers = [0.2, 0.3, 0.4, 0.5] # [0.01, 0.05, 0.1]
+    activations = ["LeakyReLU",]
+    c_ps = [0.1] # [0.05, 0.01, 0.005, 0.001]
+    lrs = [0.01] #  0.005]
 
     nz = 32
     n_d = 0
-    lr = 0.01
-
-    # for lr in [0.01, 0.05, 0.1]:
-    #     for n_g in [1000, 10000, 50000]:
-    #         args = Args(
-    #             # Model Parameters
-    #             hidden=[64], nz=nz, ngf=32, nc=1, activation="LeakyReLU",
-    #             # Privacy Parameters
-    #             epsilon=50.0, delta=1e-6, noise_multiplier=0.0, c_p=100,
-    #             # Training Parameters
-    #             lr=lr, beta1=0.5, batch_size=64, n_d=0, n_g=n_g, lambda_gp=0.0
-    #         )
-    #         main(args, latent_type="wgan")
-    #         main(args, latent_type="ae_enc")
-    #         main(args, latent_type="ae_grad")
+    n_g = 10000
+    batch_size = 64
     
-    for c_p in c_ps:
-        args = Args(
-            # Model Parameters
-            hidden=[64], nz=32, ngf=32, nc=1, activation="LeakyReLU",
-            # Privacy Parameters
-            epsilon=50.0, delta=1e-6, noise_multiplier=0.0, c_p=c_p,
-            # Training Parameters
-            lr=lr, beta1=0.5, batch_size=64, n_d=0, n_g=50000, lambda_gp=0.0
-        )
-        # main(args, latent_type="wgan")
-        # main(args, latent_type="ae_enc")
-        main(args, latent_type="ae_grad")
-
-    exit(0)
-
-    for activation, c_p, noise_multiplier in product(
-            activations, c_ps, noise_multipliers):
+    for activation, c_p, noise_multiplier, lr in product(
+            activations, c_ps, noise_multipliers, lrs):
         args = Args(
             # Model Parameters
             hidden=[64], nz=32, ngf=32, nc=1, activation=activation,
             # Privacy Parameters
             epsilon=50.0, delta=1e-6, noise_multiplier=noise_multiplier, c_p=c_p,
             # Training Parameters
-            lr=lr, beta1=0.5, batch_size=64, n_d=0, n_g=100000, lambda_gp=0.0
+            lr=lr, beta1=0.5, batch_size=batch_size, n_d=0, n_g=n_g, lambda_gp=0.0
         )
-        main(args, latent_type="ae_enc")
+        # main(args, latent_type="ae_enc")
         main(args, latent_type="ae_grad")
+    
+    exit(0)
+
+
+
+    # Private model Hyperparameter Search
+    hiddens = [64]
+    noise_multipliers = [0.2, 0.3, 0.4, 0.5] # [0.01, 0.05, 0.1]
+    activations = ["LeakyReLU", "Tanh", ]
+    c_ps = [0.5, 0.25, 0.1] # [0.05, 0.01, 0.005, 0.001]
+    lrs = [0.01] #  0.005]
+
+    nz = 32
+    n_d = 0
+    n_g = 48000
+    batch_size = 64
+    
+    for activation, c_p, noise_multiplier, lr in product(
+            activations, c_ps, noise_multipliers, lrs):
+        args = Args(
+            # Model Parameters
+            hidden=[64], nz=32, ngf=32, nc=1, activation=activation,
+            # Privacy Parameters
+            epsilon=50.0, delta=1e-6, noise_multiplier=noise_multiplier, c_p=c_p,
+            # Training Parameters
+            lr=lr, beta1=0.5, batch_size=batch_size, n_d=0, n_g=n_g, lambda_gp=0.0
+        )
+        # main(args, latent_type="ae_enc")
+        main(args, latent_type="ae_grad")
+
+
+    # Non-private
+    # # for lr in [0.01, 0.05, 0.1]:
+    # #     for n_g in [1000, 10000, 50000]:
+    
+    # for c_p in c_ps:
+    #     args = Args(
+    #         # Model Parameters
+    #         hidden=[64], nz=32, ngf=32, nc=1, activation="LeakyReLU",
+    #         # Privacy Parameters
+    #         epsilon=50.0, delta=1e-6, noise_multiplier=0.0, c_p=c_p,
+    #         # Training Parameters
+    #         lr=lr, beta1=0.5, batch_size=64, n_d=0, n_g=50000, lambda_gp=0.0
+    #     )
+    #     # main(args, latent_type="wgan")
+    #     # main(args, latent_type="ae_enc")
+    #     main(args, latent_type="ae_grad")
 
 
 if __name__ == "__main__":